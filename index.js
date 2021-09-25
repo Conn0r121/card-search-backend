3e6678b861e4a3b774e66ee6b9767c1331652928
Connor Robinson <connorrobinson121@gmail.com>
2021-09-25 14:25:49 -0400

    Adding hardcoded connection to db

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://connor:gnPY8DDKcPxgIRWn@cluster0.fabc3.mongodb.net/test');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
