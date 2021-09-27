3cbb8c8f175e2e6a9b5c2939f383a87139fd3d43
Connor Robinson <connorrobinson121@gmail.com>
2021-09-26 23:32:13 -0400

    Adding a file with a hardcoded upload script

---
+++ upload-data.js
@@ -0,0 +1,22 @@
+const oracle_cards = require('../oracle-cards-20210925090339.json');
+const mongoose = require('mongoose');
+const cardModel = require('./models/card.js');
+
+async function main() {
+    const databasePath = 'mongodb+srv://connor:gnPY8DDKcPxgIRWn@cluster0.fabc3.mongodb.net/cards';
+    mongoose.connect(databasePath, {useNewUrlParser: true, useUnifiedTopology: true});
+    const db = mongoose.connection;
+    
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    db.once('open', async () => {
+        console.log('connected');
+    
+        oracle_cards.forEach(async (cardJson) => {
+            const card = new cardModel(cardJson);
+            await card.save();
+        });
+        console.log('all cards saved!');
+    });
+}
+
+main();
