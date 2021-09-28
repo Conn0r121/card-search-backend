c72b9a737631dc859aa80815f826da2aa7b587d2
Connor Robinson <connorrobinson121@gmail.com>
2021-09-27 23:35:05 -0400

    Functional Name Query

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const mongoose = require('mongoose');
+const cardModel = require('./models/card.js');
+require('dotenv').config();
+
 const app = require('express')();
 const PORT = 8080;
+
+const databasePath = `${process.env.DATABASE_URI}/cards`;
+mongoose.connect(databasePath, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
 
 app.listen(
     PORT,
     () => console.log(`its alive on http://localhost:${PORT}`)
 );
 
-app.get('/card', (req, res) => {
-    const test = req.query.id;
-    res.status(200).send({
-        name: 'static orb',
-        id: test
-    });
+app.get('/card', async (req, res) => {
+    const cardName = req.query.name;
+    const card = await cardModel.find({ name: cardName});
+    res.status(200).send(card);
 });
