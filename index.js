53279a6cb802b684bcc28c26af53ca5e8d36e860
Connor Robinson <connorrobinson121@gmai.com>
2021-10-27 13:56:29 -0400

    staging basic lambda function

--- index.js
+++ index.js
@@ -5,20 +5,29 @@ import express from 'express';
 
 dotenv.config();
 
-const app = express();
+// const app = express();
 const PORT = 8080;
 
-const databasePath = `${process.env.DATABASE_URI}/cards`;
-mongoose.connect(databasePath, {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
+// const databasePath = `${process.env.DATABASE_URI}/cards`;
+// mongoose.connect(databasePath, {useNewUrlParser: true, useUnifiedTopology: true});
+// const db = mongoose.connection;
 
-app.listen(
-    PORT,
-    () => console.log(`its alive on http://localhost:${PORT}`)
-);
+// app.listen(
+//     PORT,
+//     () => console.log(`its alive on http://localhost:${PORT}`)
+// );
 
-app.get('/card', async (req, res) => {
-    const cardName = req.query.name;
-    const card = await cardModel.find({ name: cardName});
-    res.status(200).send(card);
-});
+// app.get('/card', async (req, res) => {
+//     const cardName = req.query.name;
+//     const card = await cardModel.find({ name: cardName});
+//     res.status(200).send(card);
+// });
+
+
+exports.handler = async (event) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+}
