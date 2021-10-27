53874907a057a0657ff0493065946ed41e761c8e
Connor Robinson <connorrobinson121@gmai.com>
2021-10-27 14:43:02 -0400

    removing unnecessary crap

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 const mongoose = require('mongoose');
 const cardModel = require('./models/card.js');
-// const dotenv = require('dotenv');
-// const fetch = require('cross-fetch');
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = 8080;
-
-// const databasePath = `${process.env.DATABASE_URI}/cards`;
-// mongoose.connect(databasePath, {useNewUrlParser: true, useUnifiedTopology: true});
-// const db = mongoose.connection;
-
-// app.listen(
-//     PORT,
-//     () => console.log(`its alive on http://localhost:${PORT}`)
-// );
-
-// app.get('/card', async (req, res) => {
-//     const cardName = req.query.name;
-//     const card = await cardModel.find({ name: cardName});
-//     res.status(200).send(card);
-// });
-
 
 exports.handler = async (event) => {
     const databasePath = `${process.env.DATABASE_URI}/cards`;
